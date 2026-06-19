584aeaac80413cfe4c8a560319a3f00babc7670d
starrynighttech <starrynight8531@gmail.com>
2026-06-19 23:36:15 +0200

    Update wallet.js

--- backend/routes/wallet.js
+++ backend/routes/wallet.js
@@ -1,1 +1,27 @@
+const router = require("express").Router()
 
+const Wallet =
+require("../models/Wallet")
+
+router.get("/:userId", async (req, res) => {
+
+  let wallet =
+  await Wallet.findOne({
+    userId: req.params.userId
+  })
+
+  if (!wallet) {
+
+    wallet =
+    await Wallet.create({
+      userId: req.params.userId,
+      balance: 0
+    })
+
+  }
+
+  res.json(wallet)
+
+})
+
+module.exports = router
