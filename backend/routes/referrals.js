8bda3e43366f717f60346a627be854208f259eca
starrynighttech <starrynight8531@gmail.com>
2026-06-20 14:44:11 +0200

    Update referrals.js

--- backend/routes/referrals.js
+++ backend/routes/referrals.js
@@ -1,1 +1,27 @@
+const router =
+require("express").Router()
 
+const Referral =
+require("../models/Referral")
+
+router.post("/", async(req,res)=>{
+
+  const referral =
+  await Referral.create(req.body)
+
+  res.status(201).json(referral)
+
+})
+
+router.get("/:userId", async(req,res)=>{
+
+  const referrals =
+  await Referral.find({
+    referrerId:req.params.userId
+  })
+
+  res.json(referrals)
+
+})
+
+module.exports = router
