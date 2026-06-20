c5556ff3635849a9c1c2c2b561e89ba3dc97db9a
starrynighttech <starrynight8531@gmail.com>
2026-06-20 15:16:20 +0200

    Update notifications.js

--- backend/routes/notifications.js
+++ backend/routes/notifications.js
@@ -1,1 +1,19 @@
+const router =
+require("express").Router()
 
+const Notification =
+require("../models/Notification")
+
+router.get("/:userId",
+async(req,res)=>{
+
+  const notifications =
+  await Notification.find({
+    userId:req.params.userId
+  })
+
+  res.json(notifications)
+
+})
+
+module.exports = router
