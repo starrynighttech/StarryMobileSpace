48b2cbd734be6e2f410ed626047cd3ea9abb1cb9
starrynighttech <starrynight8531@gmail.com>
2026-06-20 15:09:23 +0200

    Update Notification.js

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose")
 
+const NotificationSchema =
+new mongoose.Schema({
+
+  userId:String,
+
+  title:String,
+
+  message:String,
+
+  read:{
+    type:Boolean,
+    default:false
+  }
+
+},{
+  timestamps:true
+})
+
+module.exports =
+mongoose.model(
+  "Notification",
+  NotificationSchema
+)
